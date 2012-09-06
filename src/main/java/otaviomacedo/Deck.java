7f233ff9fe849de218469e989c26fcdb18ff9cf2
Otavio Macedo <otaviomacedo@gmail.com>
2012-09-06 11:31:01 -0300

    Optimization: using bit masks instead of arrays to figure out which cards should be replaced, in order to generate all possible hands.

--- src/main/java/otaviomacedo/Deck.java
+++ src/main/java/otaviomacedo/Deck.java
@@ -1,13 +1,10 @@
 package otaviomacedo;
 
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Iterables;
-import com.google.common.collect.Lists;
-
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
+import static com.google.common.collect.ImmutableList.copyOf;
 import static com.google.common.collect.Iterables.size;
 import static com.google.common.collect.Lists.newArrayList;
 import static com.google.common.collect.Sets.newHashSetWithExpectedSize;
@@ -17,12 +14,13 @@ import static com.google.common.collect.Sets.newHashSetWithExpectedSize;
  */
 public class Deck implements Iterable<Card> {
 
+    public static final int HAND_SIZE = 5;
     private final Iterable<Card> cards;
     private final Set<Iterable<Card>> allHands;
 
     public Deck(Iterable<Card> cards) {
         this.cards = cards;
-        allHands = generateAllHands(cards);
+        allHands = generateAllHands(copyOf(cards));
     }
 
     @Override
@@ -39,52 +37,31 @@ public class Deck implements Iterable<Card> {
         return allHands;
     }
 
-    private static final List<List<Integer>> REPLACEMENT_LISTS = replacementLists();
-
-    private static Set<Iterable<Card>> generateAllHands(Iterable<Card> deck) {
+    private static Set<Iterable<Card>> generateAllHands(List<Card> deck) {
         Set<Iterable<Card>> hands = newHashSetWithExpectedSize((int) Math.pow(2, size(deck)) - 1);
-        List<Card> originalHand = ImmutableList.copyOf(Iterables.limit(deck, 5));
-        List<Card> availableCards = ImmutableList.copyOf(Iterables.skip(deck, 5));
 
-        for (List<Integer> points : REPLACEMENT_LISTS) {
-            List<Card> foo = newArrayList(originalHand);
-            for (int i = 0; i < points.size(); i++) {
-                foo.set(points.get(i), availableCards.get(i));
-            }
-            hands.add(foo);
+        for (byte mask = 1; mask <= (1 << HAND_SIZE); mask++) {
+            hands.add(apply(mask, deck));
         }
 
         return hands;
     }
 
-    private static List<List<Integer>> replacementLists() {
-        List<List<Integer>> list = Lists.<List<Integer>>newArrayList(
-                newArrayList(0),
-                newArrayList(1),
-                newArrayList(2),
-                newArrayList(3),
-                newArrayList(4));
-
-        for (int i = 1; i < 5; i++) {
-            list.addAll(derive(list));
-        }
-
-        return list;
+    private static List<Card> apply(byte mask, List<Card> deck) {
+        return replace(deck.subList(0, HAND_SIZE), deck.subList(HAND_SIZE, deck.size()), mask);
     }
 
-    private static List<List<Integer>> derive(List<List<Integer>> list) {
-        List<List<Integer>> result = newArrayList();
-        for (List<Integer> sublist : list) {
-            for (int i = sublist.get(sublist.size() - 1) + 1; i < 5; i++) {
-                result.add(cons(sublist, i));
+    private static List<Card> replace(List<Card> hand, List<Card> available, byte mask){
+        List<Card> replaced = newArrayList(hand);
+
+        int i = 0, j = 0;
+
+        for (byte ref = 1 << HAND_SIZE - 1; ref > 0; ref >>= 1, j++){
+            if ((ref & mask) != 0){
+                replaced.set(j, available.get(i++));
             }
         }
-        return result;
-    }
 
-    private static List<Integer> cons(List<Integer> sublist, int i) {
-        List<Integer> temp = newArrayList(sublist);
-        temp.add(i);
-        return temp;
+        return replaced;
     }
 }
