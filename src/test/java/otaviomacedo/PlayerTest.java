a184dd5da038616bc364b2918ac9c768d2873c20
Otavio Macedo <otaviomacedo@gmail.com>
2012-09-06 13:15:33 -0300

    Verification of order in the calls to the print stream.

--- src/test/java/otaviomacedo/PlayerTest.java
+++ src/test/java/otaviomacedo/PlayerTest.java
@@ -1,11 +1,13 @@
 package otaviomacedo;
 
 import org.junit.Test;
+import org.mockito.InOrder;
 
 import java.io.IOException;
 import java.io.PrintStream;
 import java.io.StringReader;
 
+import static org.mockito.Mockito.inOrder;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
@@ -28,17 +30,18 @@ public class PlayerTest {
     public void endToEnd() throws Exception{
         player.play();
 
-        verify(stream).println("Hand: TH JH QC QD QS Deck: QH KH AH 2S 6S Best hand: straight-flush");
-        verify(stream).println("Hand: 2H 2S 3H 3S 3C Deck: 2D 3D 6C 9C TH Best hand: four-of-a-kind");
-        verify(stream).println("Hand: 2H 2S 3H 3S 3C Deck: 2D 9C 3D 6C TH Best hand: full-house");
-        verify(stream).println("Hand: 2H AD 5H AC 7H Deck: AH 6H 9H 4H 3C Best hand: flush");
-        verify(stream).println("Hand: AC 2D 9C 3S KD Deck: 5S 4D KS AS 4C Best hand: straight");
-        verify(stream).println("Hand: KS AH 2H 3C 4H Deck: KC 2C TC 2D AS Best hand: three-of-a-kind");
-        verify(stream).println("Hand: AH 2C 9S AD 3C Deck: QH KS JS JD KD Best hand: two-pairs");
-        verify(stream).println("Hand: 6C 9C 8C 2D 7C Deck: 2H TC 4C 9S AH Best hand: one-pair");
-        verify(stream).println("Hand: 3D 5S 2H QD TD Deck: 6S KH 9H AD QH Best hand: highest-card");
-    }
+        InOrder inOrder = inOrder(stream);
 
+        inOrder.verify(stream).println("Hand: TH JH QC QD QS Deck: QH KH AH 2S 6S Best hand: straight-flush");
+        inOrder.verify(stream).println("Hand: 2H 2S 3H 3S 3C Deck: 2D 3D 6C 9C TH Best hand: four-of-a-kind");
+        inOrder.verify(stream).println("Hand: 2H 2S 3H 3S 3C Deck: 2D 9C 3D 6C TH Best hand: full-house");
+        inOrder.verify(stream).println("Hand: 2H AD 5H AC 7H Deck: AH 6H 9H 4H 3C Best hand: flush");
+        inOrder.verify(stream).println("Hand: AC 2D 9C 3S KD Deck: 5S 4D KS AS 4C Best hand: straight");
+        inOrder.verify(stream).println("Hand: KS AH 2H 3C 4H Deck: KC 2C TC 2D AS Best hand: three-of-a-kind");
+        inOrder.verify(stream).println("Hand: AH 2C 9S AD 3C Deck: QH KS JS JD KD Best hand: two-pairs");
+        inOrder.verify(stream).println("Hand: 6C 9C 8C 2D 7C Deck: 2H TC 4C 9S AH Best hand: one-pair");
+        inOrder.verify(stream).println("Hand: 3D 5S 2H QD TD Deck: 6S KH 9H AD QH Best hand: highest-card");
+    }
 
     private DeckParser deckParser() {
         try {
